a8b59dfefdaa51135c57b3d8d6db5cd45c36c77c
Egbujuo Emmanuel <bunzaland@gmail.com>
2023-07-25 00:10:50 +0100

    2nd commit

---
+++ app.js
@@ -0,0 +1,189 @@
+let jobData=[
+    {
+      "id": 1,
+      "company": "Photosnap",
+      "logo": "./images/photosnap.svg",
+      "new": "NEW!",
+      "featured":"FEATURED",
+      "position": "Senior Frontend Developer",
+      "role": "Frontend",
+      "level": "Senior",
+      "postedAt": "1d ago",
+      "contract": "Full Time",
+      "location": "USA Only",
+      "languages": ["HTML", "CSS", "JavaScript"],
+      "tools": []
+    },
+    {
+      "id": 2,
+      "company": "Manage",
+      "logo": "./images/manage.svg",
+      "new": "NEW!",
+      "featured": "FEATURED!",
+      "position": "Fullstack Developer",
+      "role": "Fullstack",
+      "level": "Midweight",
+      "postedAt": "1d ago",
+      "contract": "Part Time",
+      "location": "Remote",
+      "languages": ["Python"],
+      "tools": ["React"]
+    },
+    {
+      "id": 3,
+      "company": "Account",
+      "logo": "./images/account.svg",
+      "new": "NEW!",
+      "featured":"FEATURED! ",
+      "position": "Junior Frontend Developer",
+      "role": "Frontend",
+      "level": "Junior",
+      "postedAt": "2d ago",
+      "contract": "Part Time",
+      "location": "USA Only",
+      "languages": ["JavaScript"],
+      "tools": ["React", "Sass"]
+    },
+    {
+      "id": 4,
+      "company": "MyHome",
+      "logo": "./images/myhome.svg",
+      "new": "NEW! ",
+      "featured": " FEATURED!",
+      "position": "Junior Frontend Developer",
+      "role": "Frontend",
+      "level": "Junior",
+      "postedAt": "5d ago",
+      "contract": "Contract",
+      "location": "USA Only",
+      "languages": ["CSS", "JavaScript"],
+      "tools": []
+    },
+    {
+      "id": 5,
+      "company": "Loop Studios",
+      "logo": "./images/loop-studios.svg",
+      "new": "NEW!",
+      "featured":"FEATURED!",
+      "position": "Software Engineer",
+      "role": "Fullstack",
+      "level": "Midweight",
+      "postedAt": "1w ago",
+      "contract": "Full Time",
+      "location": "Worldwide",
+      "languages": ["JavaScript"],
+      "tools": ["Ruby", "Sass"]
+    },
+    {
+      "id": 6,
+      "company": "FaceIt",
+      "logo": "./images/faceit.svg",
+      "new": "NEW!",
+      "featured":"FEATURED!",
+      "position": "Junior Backend Developer",
+      "role": "Backend",
+      "level": "Junior",
+      "postedAt": "2w ago",
+      "contract": "Full Time",
+      "location": "UK Only",
+      "languages": ["Ruby"],
+      "tools": ["RoR"]
+    },
+    {
+      "id": 7,
+      "company": "Shortly",
+      "logo": "./images/shortly.svg",
+      "new":"NEW!",
+      "featured":"FEATURED!",
+      "position": "Junior Developer",
+      "role": "Frontend",
+      "level": "Junior",
+      "postedAt": "2w ago",
+      "contract": "Full Time",
+      "location": "Worldwide",
+      "languages": ["HTML", "JavaScript"],
+      "tools": ["Sass"]
+    },
+    {
+      "id": 8,
+      "company": "Insure",
+      "logo": "./images/insure.svg",
+      "new": "NEW!",
+      "featured":"FEATURED!",
+      "position": "Junior Frontend Developer",
+      "role": "Frontend",
+      "level": "Junior",
+      "postedAt": "2w ago",
+      "contract": "Full Time",
+      "location": "USA Only",
+      "languages": ["JavaScript"],
+      "tools": ["Vue", "Sass"]
+    },
+    {
+      "id": 9,
+      "company": "Eyecam Co.",
+      "logo": "./images/eyecam-co.svg",
+      "new": "NEW!",
+      "featured": "FEATURED!",
+      "position": "Full Stack Engineer",
+      "role": "Fullstack",
+      "level": "Midweight",
+      "postedAt": "3w ago",
+      "contract": "Full Time",
+      "location": "Worldwide",
+      "languages": ["JavaScript", "Python"],
+      "tools": ["Django"]
+    },
+    {
+      "id": 10,
+      "company": "The Air Filter Company",
+      "logo": "./images/the-air-filter-company.svg",
+      "new": "NEW!",
+      "featured": "FEATURED!",
+      "position": "Front-end Dev",
+      "role": "Frontend",
+      "level": "Junior",
+      "postedAt": "1mo ago",
+      "contract": "Part Time",
+      "location": "Worldwide",
+      "languages": ["JavaScript"],
+      "tools": ["React", "Sass"]
+    }
+]
+
+  
+let Jobfunct = jobData.map((x) =>{
+
+    return `
+    <div class="list1">
+  <div class="profile">
+    <img src="${x.logo}" alt="" class="logo">
+    <div class="detail">
+      <p class="photo">${x.company}
+      <span class="new">${x.new}</span>
+      <span class="featured">${x.featured}</span></p>
+      <p class="status">${x.position}</p>
+      
+        <ul class="time">
+          <li class="no-style">${x.postedAt}</li>
+          <li>${x.contract}</li>
+          <li>${x.location}</li>
+          <li>${x.languages}</li>
+        </ul> 
+    </div>
+    
+    <ul class="credit">
+      <li>${x.role}</li>
+      <li>${x.level}</li>
+      <li>${x.languages}</li>
+      <li>${x.tools}</li>
+    </ul>
+</div>
+
+</div>
+    ` 
+})
+let jobs = document.querySelector(".listing");
+jobs.innerHTML = `${Jobfunct.join(" ")}`
+
+
